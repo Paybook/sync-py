c9eca339154a618135d0df50a9d2589df2c1c5af
Aldo <aldo.ea@hotmail.com>
2020-03-25 23:06:40 -0600

    Version 0.0.0 done with users unit test, env vars and error handler

--- src/sync/sync.py
+++ src/sync/sync.py
@@ -1,7 +1,49 @@
 # -​*- coding: utf-8 -*​-
 import requests
 
-def test():
+SYNC_API_URL = 'https://sync.paybook.com/v1'
+
+def testIP():
     response = requests.get('https://httpbin.org/ip')
     print('Your IP is {0}'.format(response.json()['origin']))
+
+class Sync():
+    @staticmethod
+    def auth(AUTH: dict, id_user: dict) -> dict:
+        testIP()
+    
+    @staticmethod
+    def strictAuth():
+        print("Stric auth mode ON!")
+        pass
+
+    @staticmethod
+    def run(AUTH: dict, route: str, payload: dict, method: str) -> dict:
+        try:
+            # BUILD URI
+            uri = SYNC_API_URL+route
+            # SET HEADERS
+            headers = {
+                'Content-type': "application/json"
+            }
+            # CHECK AUTH TYPE        
+            if 'api_key' in AUTH:
+                headers['Authorization'] = "API_KEY api_key="+AUTH['api_key']
+            elif 'token' in AUTH:
+                headers['Authorization'] = "TOKEN token="+AUTH['token']
+            else:
+                raise Exception('No valid AUTH provided')        
+            #  ASSIGN HTTP METHOD
+            headers['X-Http-Method-Override'] = {
+                'GET': 'GET',
+                'POST': 'POST',
+                'PUT': 'PUT',
+                'DELETE': 'DELETE'
+            }[method]
+
+            response = requests.post(uri, headers=headers, params=payload)
+            return response
+        except KeyError as bad_method:
+            print(f"INCORRECT METHOD ASKED: {bad_method}")            
+            raise bad_method 
 
--- test/unit_testing.py
+++ test/unit_testing.py
@@ -1,4 +1,69 @@
 # -​*- coding: utf-8 -*​-
-import sync
+import os
+import json
+import traceback
+from sync import Sync
+from dotenv import load_dotenv
+load_dotenv()
 
-sync.test()
+API_KEY = {
+    'api_key': os.getenv('API_KEY')
+}
+WEBHOOK_ENDPOINT = ''
+tmp_id_user = None
+
+def prettyPrint(parsed):
+    print(json.dumps(parsed, indent=4, sort_keys=True))
+
+def logger(parsed, message):
+    print('->', message)
+    prettyPrint(parsed)
+
+if __name__ == "__main__":
+    try:
+        # Consultar usarios
+        response = Sync.run(
+            API_KEY, 
+            '/users', 
+            None, 
+            'GET'
+        )
+        logger(response.json(), 'Consultar usuarios')
+
+        # Consultar un usuario en especifico
+        response = Sync.run(
+            API_KEY, 
+            '/users', 
+            {"id_user": '5df859c4a7a6442757726ef4'}, 
+            'GET'
+        )
+        logger(response.json(), 'Consultar un usuario en especifico')         
+        # Crear un Usuario
+        response = Sync.run(
+            API_KEY, 
+            '/users', 
+            {
+                "id_external": 'MIST030794',
+                "name": 'Rey Misterio'
+            }, 
+            'POST'
+        )
+        logger(response.json(), 'Crear un Usuario')
+        id_user = response.json()['id_user']
+        tmp_id_user = id_user
+        raise Exception('Exit')    
+    except Exception as e:
+        print('=============== UNIT TESTING ERROR ===============')
+        if tmp_id_user:
+            response = Sync.run(
+                API_KEY,
+                f"/users/{tmp_id_user}",
+                None,
+                'DELETE'
+            )
+            logger(response.json(), 'USER DELETED ON EXEPTION')
+        tb = traceback.format_exc()
+    else:
+        tb = f"FINALIZED WITH ALL TESTS PASSED"
+    finally:
+        print(tb)
